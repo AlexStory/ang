ca9e8774a68006f1429be6fbdb8032d943b2f7e2
KrisMannino <KrisMannino@gmail.com>
2014-12-05 14:39:02 -0600

    added phone# formating

--- js/main.js
+++ js/main.js
@@ -20,16 +20,32 @@ var ng = angular
           number: 'superhearing'
         }
       ];
+
       vm.addNewContact = function(){
+       vm.newContact.number = vm.formatNumber(vm.newContact.number);
         vm.contacts.push(vm.newContact);
+       
         vm.newContact = null;
+
       }
 
       vm.removeContact = function(contact){
         var i = vm.contacts.indexOf(contact);
         vm.contacts.splice(i,1);
       }
+      vm.formatNumber = function(number){
+              if(number.length == 7){
+                var newNum = number.toString().split("")
+                newNum.splice(3,0,"-");
+                return newNum.join("");
+                }else{
+                 var newNum = number.toString().split("")
+                 newNum.unshift("(");  
+                 newNum.splice(4,0,") ");
+                 newNum.splice(8,0,"-");
+                 return newNum.join("");
+                }
+          }
 
     });
-
 }());
